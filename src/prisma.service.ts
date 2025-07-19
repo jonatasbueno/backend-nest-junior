60d61bc8a6e0e2c0a86232d909331203875b6d83
Jônatas Livramento <jonatas.livramento@gmail.com>
2025-07-19 16:18:49 -0300

    feat: cria service do prisma

---
+++ src/prisma.service.ts
@@ -0,0 +1,19 @@
+/**
+ * Avaliador esse service vai ser usado para conectar ao banco de dados usando o Prisma Client.
+ * Ele implementa as interfaces OnModuleInit e OnModuleDestroy para gerenciar a conexão
+ * com o banco de dados durante o ciclo de vida do módulo.
+ */
+
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
